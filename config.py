316aefa80d9a0445c289871e3fb1413ded49e920
Wolverine <95951902+WolverinexD@users.noreply.github.com>
2022-03-15 16:46:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 import os
 
-from dotenv import load_dotenv
-
-# For Local Deploy
-if os.path.exists(".env"):
-    load_dotenv(".env")
-
 # Necessary Vars
 userpass = os.getenv("userpass")
 usernaam = os.getenv("usernaam")
